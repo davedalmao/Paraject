ea831beba14011397cfc62fe7c9f68382d50bcf7
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-16 17:47:50 +0800

    Completed sidebar, with window state buttons

--- Paraject/MVVM/Views/Windows/MainWindow.xaml.cs
+++ Paraject/MVVM/Views/Windows/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,10 +8,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Paraject
+namespace Paraject.MVVM.Views.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +20,12 @@ namespace Paraject
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
