186c3a467800716f6d4e1b344ccd613d2078d0d9
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-20 16:29:22 +0800

    created ViewModels, and registered them to app.xaml

--- Paraject/MVVM/ViewModels/MainViewModel.cs
+++ Paraject/MVVM/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Paraject.Core.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,38 @@ namespace Paraject.MVVM.ViewModels
 {
     class MainViewModel : BaseViewModel
     {
+        public NavigationCommand DashboardViewCommand { get; set; }
+        public NavigationCommand ProjectsViewCommand { get; set; }
+
+
+        public DashboardViewModel DashboardVM { get; set; }
+        public ProjectsViewModel ProjectsVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            DashboardVM = new DashboardViewModel();
+            ProjectsVM = new ProjectsViewModel();
+
+            CurrentView = DashboardVM;
+
+            DashboardViewCommand = new NavigationCommand(o =>
+            {
+                CurrentView = DashboardVM;
+            });
+
+            ProjectsViewCommand = new NavigationCommand(o =>
+            {
+                CurrentView = ProjectsVM;
+            });
+        }
     }
 }
--- Paraject/MVVM/Views/OptionsView.xaml.cs
+++ Paraject/MVVM/Views/OptionsView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +16,11 @@ using System.Windows.Shapes;
 namespace Paraject.MVVM.Views
 {
     /// <summary>
-    /// Interaction logic for SettingsView.xaml
+    /// Interaction logic for OptionsView.xaml
     /// </summary>
-    public partial class SettingsView : UserControl
+    public partial class OptionsView : UserControl
     {
-        public SettingsView()
+        public OptionsView()
         {
             InitializeComponent();
         }
