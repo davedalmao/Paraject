c1b463159a41adfb8eb9cbfd24400d2d4ba45608
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-22 15:42:24 +0800

    Finished UI design in the user input grid

--- Paraject/MVVM/Views/Windows/LoginWindow.xaml.cs
+++ Paraject/MVVM/Views/Windows/LoginWindow.xaml.cs
@@ -23,5 +23,11 @@ namespace Paraject.MVVM.Views.Windows
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
     }
 }
