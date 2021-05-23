97866fb13695a12e1958eb6a9adf99cff6ac9b36
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-23 17:31:21 +0800

    SignupWindow can now redirect to LoginWindow when Login text is clicked

--- Paraject/MVVM/ViewModels/Windows/SignupWindowViewModel.cs
+++ Paraject/MVVM/ViewModels/Windows/SignupWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Paraject.Core.Commands;
+using Paraject.MVVM.Views.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,29 @@ namespace Paraject.MVVM.ViewModels.Windows
 {
     class SignupWindowViewModel : BaseViewModel
     {
+        private DelegateCommand _loginWindowRedirectCommand;
+        public event EventHandler Closed; //The Window (LoginWindow) closes itself when this event is executed
+
+        public DelegateCommand LoginWindowRedirectCommand
+        {
+            get
+            {
+                return _loginWindowRedirectCommand ??= new DelegateCommand(ShowLoginWindow);
+            }
+        }
+
+
+        public void ShowLoginWindow()
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            Close();
+        }
+
+        //The method that executes Closed EventHandler
+        private void Close()
+        {
+            Closed?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
