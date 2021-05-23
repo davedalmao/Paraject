72efb40f4f916339e6cc30fd4ebd3fed4e5dc1b2
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-23 15:06:48 +0800

    added DelegateCommand

---
+++ Paraject/Core/Commands/DelegateCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Paraject.Core.Commands
+{
+    class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action RunCommand;
+
+        public DelegateCommand(Action runCommand)
+        {
+            RunCommand = runCommand;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            RunCommand();
+        }
+    }
+}
--- Paraject/MVVM/ViewModels/Windows/LoginWindowViewModel.cs
+++ Paraject/MVVM/ViewModels/Windows/LoginWindowViewModel.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Paraject.Core.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Paraject.MVVM.ViewModels.Windows
 {
     class LoginWindowViewModel : BaseViewModel
     {
+        public NavigationCommand SignupWindowViewCommand { get; set; }
 
     }
 }
