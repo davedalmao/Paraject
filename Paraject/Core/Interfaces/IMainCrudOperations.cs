a0ce3bffdc49f5360f4596ebbe7001c512bd0e22
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-24 14:16:02 +0800

    created an interface for main CRUD operations (IMainCrudOperations.cs)

---
+++ Paraject/Core/Interfaces/IMainCrudOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paraject.Core.Interfaces
+{
+    public interface IMainCrudOperations<TEntity>
+    {
+        public TEntity Get(int id);
+        public void Add(TEntity entity);
+        public void Update(TEntity entity);
+        public void Delete(int id);
+    }
+}
---
+++ Paraject/Core/Services/UserCrudService.cs
@@ -0,0 +1,32 @@
+﻿using Paraject.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paraject.Core.Services
+{
+    class UserCrudService<TEntity> : IMainCrudOperations<TEntity>
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
