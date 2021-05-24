f2263a916f48cc2ff8ea7b539ac7b07f6e05b3b7
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-24 14:19:24 +0800

    Added IFindAll and IDisplayAll interface

---
+++ Paraject/Core/Interfaces/IFindAll.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paraject.Core.Interfaces
+{
+    public interface IFindAll<TEntity>
+    {
+        public IEnumerable<TEntity> FindAll(string storedProcedure);
+    }
+}
