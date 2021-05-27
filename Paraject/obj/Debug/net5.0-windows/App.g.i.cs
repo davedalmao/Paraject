5faa2534b5a652519f6e7881c70e52bb4b4611ab
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-27 17:02:42 +0800

    created UserAccountRepository instance in the UserAccountViewModel constructor

--- Paraject/obj/Debug/net5.0-windows/App.g.i.cs
+++ Paraject/obj/Debug/net5.0-windows/App.g.i.cs
@@ -63,7 +63,7 @@ namespace Paraject {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Paraject;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Paraject;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
