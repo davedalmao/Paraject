92946a869507cd9a00b4ecf68dd86f83530052da
John Dave Dalmao <johndavedalmao@gmail.com>
2021-05-27 16:04:58 +0800

    changed CustomControlClasses folder to CustomControls

--- Paraject/Core/CustomControls/MenuItem.cs
+++ Paraject/Core/CustomControls/MenuItem.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
-namespace Paraject.Core.CustomControlClasses
+namespace Paraject.Core.CustomControls
 {
-    /// <summary>
-    /// Sidebar menu item
-    /// </summary>
-    class MenuItem : RadioButton
+    public class MenuItem : RadioButton
     {
         public string DefaultImageUriString
         {
