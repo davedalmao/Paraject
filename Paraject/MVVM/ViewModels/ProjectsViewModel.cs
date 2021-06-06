d9039dc47de29ee547f55aff7013090dbb946a7c
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-06 14:49:04 +0800

    refactored ProjectsViewModel (not yet the final code)

--- Paraject/MVVM/ViewModels/ProjectsViewModel.cs
+++ Paraject/MVVM/ViewModels/ProjectsViewModel.cs
@@ -5,14 +5,11 @@ namespace Paraject.MVVM.ViewModels
 {
     public class ProjectsViewModel : BaseViewModel
     {
-        //public object CurrentView { get; set; }
-
-        public ICommand DisplayProjectsViewCommand { get; }
         public ICommand AllProjectsCommand { get; }
         public ICommand PersonalProjectsCommand { get; }
         public ICommand PaidProjectsCommand { get; }
 
-        public string Message { get; set; }
+        public string Message { get; set; } //test property
 
         public DisplayProjectsViewModel DisplayProjectsVM { get; set; }
 
@@ -20,46 +17,24 @@ namespace Paraject.MVVM.ViewModels
         {
             DisplayProjectsVM = new DisplayProjectsViewModel();
 
-            AllProjectsCommand = new DelegateCommand(all);
-            PersonalProjectsCommand = new DelegateCommand(personal);
-            PaidProjectsCommand = new DelegateCommand(paid);
+            AllProjectsCommand = new DelegateCommand(AllProjects);
+            PersonalProjectsCommand = new DelegateCommand(PersonalProjects);
+            PaidProjectsCommand = new DelegateCommand(PaidProjects);
 
-            all();
+            AllProjects();
         }
 
-        public void all()
+        public void AllProjects()
         {
-            //MessageBox.Show("all");
             Message = "all";
         }
-        public void personal()
+        public void PersonalProjects()
         {
-            //MessageBox.Show("personal");
             Message = "personal";
         }
-        public void paid()
+        public void PaidProjects()
         {
-            //MessageBox.Show("paid");
             Message = "paid";
         }
-
-
-        /*
-        if (all projects radio button is selected)
-        {
-            GetAll();
-            background = red;
-        }
-        else if (personal projects radio button is selected)
-        {
-            FindAll(Personal);
-            background = yellow;
-        }
-        else if (paid projects radio button is selected)
-        {
-            FindAll(paid);
-            background = blue;
-        }
-        */
     }
 }
