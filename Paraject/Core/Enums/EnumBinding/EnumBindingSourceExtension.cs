1e09b68d60f22ec9f139083f2f92a6895cac39f1
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-06 17:41:08 +0800

    bind enum (ProjectOptions) to a ComboBox

---
+++ Paraject/Core/Enums/EnumBinding/EnumBindingSourceExtension.cs
@@ -0,0 +1,28 @@
+﻿using Paraject.Core.Repositories;
+using System;
+using System.Windows.Markup;
+
+namespace Paraject.Core.Enums.EnumBinding
+{
+    /// <summary>
+    /// This class is used for BINDING Enums
+    /// </summary>
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        public Type EnumType { get; private set; }
+        public ProjectOptions ProjectOptions { get; set; }
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            if (enumType is not null || enumType.IsEnum)
+            {
+                EnumType = enumType;
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
