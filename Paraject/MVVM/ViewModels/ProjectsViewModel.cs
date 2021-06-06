289313549d3e4a1f307d8b99de7c8059f9dff296
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-06 14:18:25 +0800

    created DisplayProjectsView and DisplayProjectsViewModel and registered them in App.xaml (datatemplate)

--- Paraject/MVVM/ViewModels/ProjectsViewModel.cs
+++ Paraject/MVVM/ViewModels/ProjectsViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace Paraject.MVVM.ViewModels
+﻿using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels
 {
-    public class ProjectsViewModel
+    public class ProjectsViewModel : BaseViewModel
     {
+        public object CurrentView { get; set; }
+        public ICommand DisplayProjectsViewCommand { get; }
+
+
+        /*
+        if (all projects radio button is selected)
+        {
+            GetAll();
+            background = red;
+        }
+        else if (personal projects radio button is selected)
+        {
+            FindAll(Personal);
+            background = yellow;
+        }
+        else if (paid projects radio button is selected)
+        {
+            FindAll(paid);
+            background = blue;
+        }
+        */
     }
 }
