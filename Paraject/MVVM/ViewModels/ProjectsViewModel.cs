6c771a94e795b9a068f0a32f1b6d671835b18903
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-06 14:46:52 +0800

    shared DataContext (ProjectsViewModel) to a UserControl(ProjectsView) inside another UserControl(DisplayProjectsView), and navigated to all menu items in ProjectsView

--- Paraject/MVVM/ViewModels/ProjectsViewModel.cs
+++ Paraject/MVVM/ViewModels/ProjectsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using Paraject.Core.Commands;
+using System.Windows.Input;
 
 namespace Paraject.MVVM.ViewModels
 {
@@ -7,13 +8,41 @@ namespace Paraject.MVVM.ViewModels
         //public object CurrentView { get; set; }
 
         public ICommand DisplayProjectsViewCommand { get; }
+        public ICommand AllProjectsCommand { get; }
+        public ICommand PersonalProjectsCommand { get; }
+        public ICommand PaidProjectsCommand { get; }
+
+        public string Message { get; set; }
 
         public DisplayProjectsViewModel DisplayProjectsVM { get; set; }
 
         public ProjectsViewModel()
         {
             DisplayProjectsVM = new DisplayProjectsViewModel();
+
+            AllProjectsCommand = new DelegateCommand(all);
+            PersonalProjectsCommand = new DelegateCommand(personal);
+            PaidProjectsCommand = new DelegateCommand(paid);
+
+            all();
         }
+
+        public void all()
+        {
+            //MessageBox.Show("all");
+            Message = "all";
+        }
+        public void personal()
+        {
+            //MessageBox.Show("personal");
+            Message = "personal";
+        }
+        public void paid()
+        {
+            //MessageBox.Show("paid");
+            Message = "paid";
+        }
+
 
         /*
         if (all projects radio button is selected)
