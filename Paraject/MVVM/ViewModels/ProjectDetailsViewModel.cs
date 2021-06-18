81006638e69850267aedef2f021f88bb19fbb646
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-18 15:11:58 +0800

    implemented update functionality for Project (in ProjectDetailsViewModel)

--- Paraject/MVVM/ViewModels/ProjectDetailsViewModel.cs
+++ Paraject/MVVM/ViewModels/ProjectDetailsViewModel.cs
@@ -1,21 +1,41 @@
 ﻿using Microsoft.Win32;
 using Paraject.Core.Commands;
+using Paraject.Core.Repositories;
 using Paraject.MVVM.Models;
 using System.Drawing;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Paraject.MVVM.ViewModels
 {
     public class ProjectDetailsViewModel : BaseViewModel
     {
+        private readonly ProjectRepository _projectRepository;
         public ProjectDetailsViewModel(Project currentProject)
         {
+            //Repository
+            _projectRepository = new ProjectRepository();
+
+            //Project Model
             CurrentProject = currentProject;
+
+            //Commands
             AddOrChangeLogoCommand = new DelegateCommand(LoadProjectLogo);
+            UpdateProjectCommand = new DelegateCommand(Update);
+            DeleteProjectCommand = new DelegateCommand(Delete);
         }
 
+        #region Properties
+        //Model
         public Project CurrentProject { get; set; }
+
+        //Commands
         public ICommand AddOrChangeLogoCommand { get; }
+        public ICommand UpdateProjectCommand { get; }
+        public ICommand DeleteProjectCommand { get; }
+        #endregion
+
+        #region Methods
         private void LoadProjectLogo()
         {
             OpenFileDialog openFile = new OpenFileDialog
@@ -31,5 +51,29 @@ namespace Paraject.MVVM.ViewModels
                 CurrentProject.Logo = Image.FromFile(openFile.FileName);
             }
         }
+        private void Update()
+        {
+            if (!string.IsNullOrWhiteSpace(CurrentProject.Name))
+            {
+                bool isUpdated = _projectRepository.Update(CurrentProject);
+                if (isUpdated)
+                {
+                    MessageBox.Show("Project updated successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Error occured, cannot update the project");
+                }
+            }
+            else
+            {
+                MessageBox.Show("A Project should have a name");
+            }
+        }
+        private void Delete()
+        {
+            MessageBox.Show("Delete");
+        }
+        #endregion
     }
 }
