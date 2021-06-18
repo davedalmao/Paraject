6eb7b1b0d6221aa8cd1847f13772e2de15429d07
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-18 14:38:47 +0800

    bound all Project details/properties with their respective controls in ProjectDetailsView

--- Paraject/MVVM/ViewModels/ProjectDetailsViewModel.cs
+++ Paraject/MVVM/ViewModels/ProjectDetailsViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace Paraject.MVVM.ViewModels
+﻿using Paraject.MVVM.Models;
+
+namespace Paraject.MVVM.ViewModels
 {
     public class ProjectDetailsViewModel : BaseViewModel
     {
+        public ProjectDetailsViewModel(Project currentProject)
+        {
+            CurrentProject = currentProject;
+        }
+
+        public Project CurrentProject { get; set; }
     }
 }
