9041756679f75fa07e3e80da172ec3fa3abe19e4
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-18 16:46:57 +0800

    change userId method parameters to projectId to avoid naming discrepancies

--- Paraject/Core/Repositories/Interfaces/IProjectRepository.cs
+++ Paraject/Core/Repositories/Interfaces/IProjectRepository.cs
@@ -6,11 +6,11 @@ namespace Paraject.Core.Repositories.Interfaces
     interface IProjectRepository
     {
         public bool Add(Project project, int userId);
-        public Project Get(int userId);
+        public Project Get(int projectId);
         public IEnumerable<Project> GetAll(int userId);
         public IEnumerable<Project> FindAll(int userId, ProjectOptions projectOption);
         public bool Update(Project project);
-        public bool Delete(int userId);
+        public bool Delete(int projectId);
 
     }
 }
