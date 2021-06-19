011f2c675f198f2d9d05f0be280f3de6f77e7fbc
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-19 18:56:43 +0800

    implemented the back button functionality in TasksView (redirects to ProjectView)

--- Paraject/MVVM/ViewModels/TasksViewModel.cs
+++ Paraject/MVVM/ViewModels/TasksViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using Paraject.Core.Commands;
+using Paraject.Core.Repositories;
 using Paraject.MVVM.Models;
+using Paraject.MVVM.ViewModels.Windows;
 using System.Windows.Input;
 
 namespace Paraject.MVVM.ViewModels
 {
     public class TasksViewModel : BaseViewModel
     {
+        private readonly UserAccountRepository _userAccountRepository;
+
         public TasksViewModel(Project currentProject)
         {
+            _userAccountRepository = new UserAccountRepository();
+
             TasksTodoVM = new TasksTodoViewModel();
             CompletedTasksVM = new CompletedTasksViewModel();
             ProjectNotesVM = new ProjectNotesViewModel();
@@ -15,10 +21,12 @@ namespace Paraject.MVVM.ViewModels
 
             CurrentView = TasksTodoVM;
 
+            //TasksView child Views
             TasksTodoViewCommand = new ParameterizedDelegateCommand(o => { CurrentView = TasksTodoVM; });
             CompletedTasksViewCommand = new ParameterizedDelegateCommand(o => { CurrentView = CompletedTasksVM; });
             ProjectNotesViewCommand = new ParameterizedDelegateCommand(o => { CurrentView = ProjectNotesVM; });
             ProjectDetailsViewCommand = new ParameterizedDelegateCommand(o => { CurrentView = ProjectDetailsVM; });
+            ProjectsViewCommand = new DelegateCommand(NavigateToProjectsView);
 
             CurrentProject = currentProject;
         }
@@ -27,6 +35,9 @@ namespace Paraject.MVVM.ViewModels
         public object CurrentView { get; set; }
 
         #region ViewModels (that will navigate with their associated Views)
+        public ProjectsViewModel ProjectsVM { get; set; }
+
+        //TasksView child Views
         public TasksTodoViewModel TasksTodoVM { get; set; }
         public CompletedTasksViewModel CompletedTasksVM { get; set; }
         public ProjectNotesViewModel ProjectNotesVM { get; set; }
@@ -34,10 +45,20 @@ namespace Paraject.MVVM.ViewModels
         #endregion
 
         #region Commands
+        public ICommand ProjectsViewCommand { get; }
         public ICommand TasksTodoViewCommand { get; }
         public ICommand CompletedTasksViewCommand { get; }
         public ICommand ProjectNotesViewCommand { get; }
         public ICommand ProjectDetailsViewCommand { get; }
         #endregion
+
+        #region Navigation Methods
+        public void NavigateToProjectsView()
+        {
+            ProjectsVM = new ProjectsViewModel(_userAccountRepository.GetById(CurrentProject.User_Id_Fk));
+            MainWindowViewModel.CurrentView = ProjectsVM;
+        }
+        #endregion
+
     }
 }
