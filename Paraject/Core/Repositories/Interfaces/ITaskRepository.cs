5a444fdb73e0827fbae14d074e827310995b4655
John Dave Dalmao <johndavedalmao@gmail.com>
2021-06-25 14:34:02 +0800

    completed ITaskRepository

--- Paraject/Core/Repositories/Interfaces/ITaskRepository.cs
+++ Paraject/Core/Repositories/Interfaces/ITaskRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿
+using Paraject.Core.Enums;
+using Paraject.MVVM.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Paraject.Core.Repositories.Interfaces
 {
-    interface ITaskRepository
+    public interface ITaskRepository
     {
+        public bool Add(Task task, int projectId);
+        public Task Get(int taskId);
+        public IEnumerable<Task> FindAll(int projectId, Status taskStatus, Priority taskPriority, Category taskCategory);
+        public bool Update(Task task);
+        public bool Delete(int taskId);
     }
 }
