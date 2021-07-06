9309577e3c41618e669cadeabd8d548ce31d99a0
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-06 13:48:46 +0800

    finished creating the interface for SubtaskRepository (ISubtaskRepository)

---
+++ Paraject/Core/Repositories/Interfaces/ISubtaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using Paraject.MVVM.Models;
+using System.Collections.Generic;
+
+namespace Paraject.Core.Repositories.Interfaces
+{
+    public interface ISubtaskRepository
+    {
+        public bool Add(Subtask subtask, int taskId);
+        public Subtask Get(int subtaskId);
+        public IEnumerable<Subtask> GetAll(int taskId);
+        public IEnumerable<Subtask> FindAll(int taskId, string subtaskStatus, string subtaskPriority);
+        public bool Update(Subtask subtask);
+        public bool Delete(int subtaskId);
+    }
+}
