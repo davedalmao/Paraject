e071711d5e3d8d6c8568e1767545e305f68442c8
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-06 15:12:50 +0800

    AddSubtaskModalDialog is now shown when Add subtask button is clicked (in TaskDetailsView)

--- Paraject/obj/Debug/net5.0-windows/MVVM/Views/ModalDialogs/AddSubtaskModalDialog.g.i.cs
+++ Paraject/obj/Debug/net5.0-windows/MVVM/Views/ModalDialogs/AddSubtaskModalDialog.g.i.cs
@@ -62,7 +62,7 @@ namespace Paraject.MVVM.Views.ModalDialogs {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Paraject;V1.0.0.0;component/mvvm/views/modaldialogs/addsubtaskmodaldialog.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Paraject;component/mvvm/views/modaldialogs/addsubtaskmodaldialog.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\MVVM\Views\ModalDialogs\AddSubtaskModalDialog.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
