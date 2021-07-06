358a6b58d29e897a9d1057386870e7befac5404f
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-06 15:15:34 +0800

    implemented close modal functionality for AddSubtaskModalDialog

--- Paraject/MVVM/ViewModels/ModalDialogs/AddSubtaskModalDialogViewModel.cs
+++ Paraject/MVVM/ViewModels/ModalDialogs/AddSubtaskModalDialogViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace Paraject.MVVM.ViewModels.ModalDialogs
+﻿using Paraject.Core.Commands;
+using Paraject.MVVM.ViewModels.Windows;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels.ModalDialogs
 {
     public class AddSubtaskModalDialogViewModel : BaseViewModel
     {
+        public AddSubtaskModalDialogViewModel()
+        {
+            CloseModalDialogCommand = new DelegateCommand(CloseModalDialog);
+        }
+
+        public ICommand CloseModalDialogCommand { get; }
+
+        private void CloseModalDialog()
+        {
+            MainWindowViewModel.Overlay = false;
+
+            foreach (Window currentModal in Application.Current.Windows)
+            {
+                if (currentModal.DataContext == this)
+                {
+                    currentModal.Close();
+                }
+            }
+        }
     }
 }
