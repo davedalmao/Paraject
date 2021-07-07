b8c0673c5169fdab4b59eea6bafcae5d6407b514
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-07 14:02:34 +0800

    successfully navigated back to TaskDetailsView when back button is clicked in SubtasksView

--- Paraject/MVVM/ViewModels/SubtasksViewModel.cs
+++ Paraject/MVVM/ViewModels/SubtasksViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace Paraject.MVVM.ViewModels
+﻿using Paraject.Core.Commands;
+using Paraject.MVVM.ViewModels.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels
 {
     public class SubtasksViewModel : BaseViewModel
     {
+        private readonly TaskDetailsViewModel _taskDetailsViewModel;
+
+        public SubtasksViewModel(TaskDetailsViewModel taskDetailsViewModel)
+        {
+            NavigateBackToTaskDetailsViewCommand = new DelegateCommand(NavigateBackToTaskDetailsView);
+            _taskDetailsViewModel = taskDetailsViewModel;
+        }
+
+        public ICommand NavigateBackToTaskDetailsViewCommand { get; }
+
+        public void NavigateBackToTaskDetailsView()
+        {
+            MainWindowViewModel.CurrentView = _taskDetailsViewModel;
+        }
     }
 }
