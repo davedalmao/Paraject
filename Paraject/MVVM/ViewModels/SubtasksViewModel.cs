ec2646f8759f261506c85e6621fe1f1e9387f023
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-07 14:22:10 +0800

    finished  setting up the grid (in the View: SubtasksView and ViewModel:SubtasksViewModel) that displays the subtasks

--- Paraject/MVVM/ViewModels/SubtasksViewModel.cs
+++ Paraject/MVVM/ViewModels/SubtasksViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Paraject.Core.Commands;
+using Paraject.MVVM.Models;
 using Paraject.MVVM.ViewModels.Windows;
-using System.Windows;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace Paraject.MVVM.ViewModels
@@ -15,9 +16,12 @@ namespace Paraject.MVVM.ViewModels
             NavigateBackToTaskDetailsViewCommand = new DelegateCommand(NavigateBackToTaskDetailsView);
             FilterTasksCommand = new DelegateCommand(DisplayAllFilteredTasks);
         }
+        public ObservableCollection<Subtask> Subtasks { get; set; }
+        public ObservableCollection<GridTileData> CardSubtasksGrid { get; set; }
 
         public string StatusFilter { get; set; } = "Show All";
         public string PriorityFilter { get; set; } = "Show All";
+
         public ICommand NavigateBackToTaskDetailsViewCommand { get; }
         public ICommand FilterTasksCommand { get; }
 
@@ -27,9 +31,50 @@ namespace Paraject.MVVM.ViewModels
             MainWindowViewModel.CurrentView = _taskDetailsViewModel;
         }
 
+        private void GetValuesForSubtasksCollection()
+        {
+            Subtasks = null;
+            //Subtasks = new ObservableCollection<Task>(_taskRepository.FindAll(_projectId, _currentTaskType, StatusFilter, PriorityFilter, CategoryFilter)
+            //                                                      .Where(task => task.Status != "Completed"));
+        }
+        private void SetNewGridDisplay()
+        {
+            CardSubtasksGrid = null;
+            CardSubtasksGrid = new ObservableCollection<GridTileData>();
+        }
+        private void SubtaskCardGridLocation()
+        {
+            int row = -1;
+            int column = -1;
+
+            //This is for a 3 column grid, with n number of rows
+            for (int i = 0; i < Subtasks.Count; i++)
+            {
+                if (column == 2)
+                {
+                    column = 0;
+                }
+
+                else
+                {
+                    column++;
+                }
+
+                if (i % 3 == 0)
+                {
+                    row++;
+                }
+
+                GridTileData td = new(Subtasks[i], row, column);
+                CardSubtasksGrid.Add(td);
+            }
+        }
         private void DisplayAllFilteredTasks()
         {
-            MessageBox.Show($"Status: {StatusFilter} \nPriority: {PriorityFilter}");
+            //MessageBox.Show($"Status: {StatusFilter} \nPriority: {PriorityFilter}");
+            GetValuesForSubtasksCollection();
+            SetNewGridDisplay();
+            SubtaskCardGridLocation();
         }
     }
 }
