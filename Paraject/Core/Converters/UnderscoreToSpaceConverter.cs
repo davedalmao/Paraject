397f64b1ba7456a0d3b98bcb5a5e3bdbd718d190
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-11 13:34:42 +0800

    added UnderscoreToSpaceConverter in Converters folder

---
+++ Paraject/Core/Converters/UnderscoreToSpaceConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Paraject.Core.Converters
+{
+    public class UnderscoreToSpaceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToString(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = System.Convert.ToString(value);
+
+            text = text.Replace("_", " ");
+
+            return text;
+        }
+    }
+}
