d6a652d9ed808a11f78a3ef7f11d7aea78beacda
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-11 16:54:53 +0800

    implemented Update method in NoteRepository

--- Paraject/Core/Repositories/NoteRepository.cs
+++ Paraject/Core/Repositories/NoteRepository.cs
@@ -168,7 +168,34 @@ namespace Paraject.Core.Repositories
         }
         public bool Update(Note note)
         {
-            throw new NotImplementedException();
+            bool isUpdated = false;
+
+            using (SqlConnection con = new(_connectionString))
+            using (SqlCommand cmd = new("Note.spUpdateNote", con))
+            {
+                try
+                {
+                    con.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@note_id", SqlDbType.Int).Value = note.Id;
+                    cmd.Parameters.Add("@note_subject", SqlDbType.NVarChar, 50).Value = note.Subject;
+                    cmd.Parameters.Add("@note_description", SqlDbType.NVarChar, 1515).Value = string.IsNullOrWhiteSpace(note.Description) ? null : note.Description;
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    isUpdated = rowsAffected > 0;
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"An SQL error occured while processing data. \nError: { ex.Message }");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+
+            return isUpdated;
         }
         public bool Delete(int noteId)
         {
