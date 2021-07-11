d8a238e504fc89c6e6cc70aac9d5ae41eb066eb3
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-11 13:51:08 +0800

    registered UnderscoreToSpaceConverter in App.xaml, and updated the SubtaskView's header if the selected Task's category changes (in TasksDetailsView)

--- Paraject/Core/Converters/UnderscoreToSpaceConverter.cs
+++ Paraject/Core/Converters/UnderscoreToSpaceConverter.cs
@@ -4,20 +4,19 @@ using System.Windows.Data;
 
 namespace Paraject.Core.Converters
 {
+    /// <summary>
+    /// This is used in SubtasksView's header
+    /// </summary>
     public class UnderscoreToSpaceConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return System.Convert.ToString(value);
+            return $"[ {value.ToString().Replace("_", " ")} ]";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            string text = System.Convert.ToString(value);
-
-            text = text.Replace("_", " ");
-
-            return text;
+            return null;
         }
     }
 }
