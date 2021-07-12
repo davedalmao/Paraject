b28c0073f21ecc514075ee1826618c8ad8b51c07
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-12 14:20:06 +0800

    NoteModalDialog is now shown if Add New Note button is clicked in NotesView

--- Paraject/MVVM/ViewModels/NotesViewModel.cs
+++ Paraject/MVVM/ViewModels/NotesViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace Paraject.MVVM.ViewModels
+﻿using Paraject.Core.Commands;
+using Paraject.MVVM.ViewModels.Windows;
+using Paraject.MVVM.Views.ModalDialogs;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels
 {
     public class NotesViewModel : BaseViewModel
     {
+        public NotesViewModel()
+        {
+            ShowNoteModalDialogCommand = new DelegateCommand(ShowNoteModalDialog);
+        }
+
+        public ICommand ShowNoteModalDialogCommand { get; }
+
+        public void ShowNoteModalDialog()
+        {
+            MainWindowViewModel.Overlay = true;
+
+            NoteModalDialog noteModalDialog = new NoteModalDialog();
+            noteModalDialog.ShowDialog();
+        }
     }
 }
