53b8d82533226fc9537a80080311fe5d87469f80
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-13 14:10:17 +0800

    created NoteDetailsModalDialogViewModel with  close modal functionality

--- Paraject/MVVM/ViewModels/ModalDialogs/NoteDetailsModalDialogViewModel.cs
+++ Paraject/MVVM/ViewModels/ModalDialogs/NoteDetailsModalDialogViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace Paraject.MVVM.ViewModels.ModalDialogs
+﻿using Paraject.Core.Commands;
+using Paraject.MVVM.ViewModels.Windows;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels.ModalDialogs
 {
     public class NoteDetailsModalDialogViewModel : BaseViewModel
     {
+        public NoteDetailsModalDialogViewModel()
+        {
+            CloseModalDialogCommand = new DelegateCommand(CloseModalDialog);
+        }
+
+        public ICommand CloseModalDialogCommand { get; }
+
+        private void CloseModalDialog()
+        {
+            MainWindowViewModel.Overlay = false;
+
+            foreach (Window currentModal in Application.Current.Windows)
+            {
+                if (currentModal.DataContext == this)
+                {
+                    currentModal.Close();
+                }
+            }
+        }
+
     }
 }
