7456ec1ed8f68ca12806a7897966cd263e205fe3
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-13 14:00:24 +0800

    Completed the UI design for NoteDetailsModalDialog

---
+++ Paraject/MVVM/Views/ModalDialogs/NoteDetailsModalDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.Views.ModalDialogs
+{
+    /// <summary>
+    /// Interaction logic for NoteDetailsModalDialog.xaml
+    /// </summary>
+    public partial class NoteDetailsModalDialog : Window
+    {
+        public NoteDetailsModalDialog()
+        {
+            InitializeComponent();
+            this.Owner = Application.Current.MainWindow;
+        }
+
+        private void NoteDetailsModalDialogMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) { DragMove(); }
+        }
+    }
+}
