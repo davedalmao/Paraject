3415a32055b2f7068f40d4a95736631ef913d075
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-13 13:33:29 +0800

    Completed the UI design for AddNoteModalDialog

---
+++ Paraject/MVVM/Views/ModalDialogs/AddNoteModalDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.Views.ModalDialogs
+{
+    /// <summary>
+    /// Interaction logic for AddNoteModalDialog.xaml
+    /// </summary>
+    public partial class AddNoteModalDialog : Window
+    {
+        public AddNoteModalDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNoteModalDialogMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) { DragMove(); }
+        }
+    }
+}
