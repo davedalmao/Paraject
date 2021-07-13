a52b9d9635cb6be06e52eb51c3d9d534cd22ca2c
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-13 13:44:57 +0800

    create AddNoteModalDialogViewModel with close modal dialog and add note functionality

---
+++ Paraject/MVVM/ViewModels/ModalDialogs/AddNoteModalDialogViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Paraject.Core.Commands;
+using Paraject.Core.Repositories;
+using Paraject.MVVM.Models;
+using Paraject.MVVM.ViewModels.Windows;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels.ModalDialogs
+{
+    public class AddNoteModalDialogViewModel : BaseViewModel
+    {
+        private readonly NoteRepository _noteRepository;
+        private readonly Action _refreshNotesCollection;
+        private readonly int _currentProjectId;
+
+        public AddNoteModalDialogViewModel(Action refreshNotesCollection, int currentProjectId)
+        {
+            _noteRepository = new NoteRepository();
+            _refreshNotesCollection = refreshNotesCollection;
+            _currentProjectId = currentProjectId;
+            CurrentNote = new Note();
+
+            CloseModalDialogCommand = new DelegateCommand(CloseModalDialog);
+            AddNoteCommand = new DelegateCommand(Add);
+        }
+
+        #region Properties
+        public Note CurrentNote { get; set; }
+
+        public ICommand AddNoteCommand { get; }
+        public ICommand CloseModalDialogCommand { get; }
+        #endregion
+
+        #region Methods
+        public void Add()
+        {
+            if (!string.IsNullOrWhiteSpace(CurrentNote.Subject))
+            {
+                bool isAdded = _noteRepository.Add(CurrentNote, _currentProjectId);
+                AddOperationResult(isAdded);
+            }
+
+            else
+            {
+                MessageBox.Show("A note should have a subject");
+            }
+        }
+        private void AddOperationResult(bool isAdded)
+        {
+            if (isAdded)
+            {
+                //refresh Notes collection in NotesView//
+                _refreshNotesCollection();
+                MessageBox.Show("Note Created");
+                CloseModalDialog();
+            }
+
+            else
+            {
+                MessageBox.Show("Error occured, cannot create note");
+            }
+        }
+
+        private void CloseModalDialog()
+        {
+            MainWindowViewModel.Overlay = false;
+
+            foreach (Window currentModal in Application.Current.Windows)
+            {
+                if (currentModal.DataContext == this)
+                {
+                    currentModal.Close();
+                }
+            }
+        }
+        #endregion
+    }
+}
