ff5b967be8fba20d1d4312a886d83000bcfbd526
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-13 16:20:05 +0800

    added the methods needed for IProjectIdeaRepository

---
+++ Paraject/Core/Repositories/Interfaces/IProjectIdeaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Paraject.MVVM.Models;
+using System.Collections.Generic;
+
+namespace Paraject.Core.Repositories.Interfaces
+{
+    public interface IProjectIdeaRepository
+    {
+        public bool Add(ProjectIdea projectIdea, int userId);
+        public ProjectIdea Get(int projectIdeaId);
+        public IEnumerable<ProjectIdea> GetAll(int userId);
+        public bool Update(ProjectIdea projectIdea);
+        public bool Delete(int projectIdeaId);
+    }
+}
