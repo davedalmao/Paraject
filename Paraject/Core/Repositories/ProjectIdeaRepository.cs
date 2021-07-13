99409d5fc22b7eaed8e80b58449972ed10cdf377
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-13 16:35:54 +0800

    finished implementing Get method in ProjectIdeaRepository

--- Paraject/Core/Repositories/ProjectIdeaRepository.cs
+++ Paraject/Core/Repositories/ProjectIdeaRepository.cs
@@ -54,7 +54,61 @@ namespace Paraject.Core.Repositories
         }
         public ProjectIdea Get(int projectIdeaId)
         {
-            throw new NotImplementedException();
+            ProjectIdea projectIdea = null;
+
+            using (SqlConnection con = new(_connectionString))
+            using (SqlCommand cmd = new("Project_Idea.spGetProjectIdea", con))
+            {
+                try
+                {
+                    con.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@project_idea_id", SqlDbType.Int).Value = projectIdeaId;
+
+                    SqlDataReader sqlDataReader = cmd.ExecuteReader();
+                    if (sqlDataReader.HasRows)
+                    {
+                        //Move to the first record.  If no records, get out.
+                        if (!sqlDataReader.Read()) { return null; }
+
+                        //Ordinals (Gets the column number from the database based on the [column name] passed in GetOrdinal method)
+                        int projectIdeaIdFromDb = sqlDataReader.GetOrdinal("project_idea_id");
+                        int userIdFk = sqlDataReader.GetOrdinal("user_id");
+                        int projectIdeaName = sqlDataReader.GetOrdinal("project_idea_name");
+                        int projectIdeaDescription = sqlDataReader.GetOrdinal("project_idea_description");
+                        int projectIdeaFeatures = sqlDataReader.GetOrdinal("project_idea_features");
+                        int dateCreated = sqlDataReader.GetOrdinal("date_created");
+
+                        //Reads a single ProjectIdea
+                        //Remember, we're already on the first record, so use do/while here.
+                        do
+                        {
+                            projectIdea = new ProjectIdea()
+                            {
+                                Id = sqlDataReader.GetInt32(projectIdeaIdFromDb),
+                                User_Id_Fk = sqlDataReader.GetInt32(userIdFk),
+                                Name = sqlDataReader.GetString(projectIdeaName),
+                                Description = sqlDataReader.GetString(projectIdeaDescription),
+                                Features = sqlDataReader.GetString(projectIdeaFeatures),
+                                DateCreated = sqlDataReader.GetDateTime(dateCreated)
+                            };
+                        }
+                        while (sqlDataReader.Read());
+                    }
+                    sqlDataReader.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"An SQL error occured while processing data. \nError: { ex.Message }");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+
+            return projectIdea;
         }
         public IEnumerable<ProjectIdea> GetAll(int userId)
         {
