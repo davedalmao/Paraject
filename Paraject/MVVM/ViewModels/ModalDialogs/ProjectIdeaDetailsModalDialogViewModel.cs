f74afeb961faa482a274c31f8a2c20370a151301
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-14 16:26:31 +0800

    displayed details of a project idea in ProjectIdeaDetailsModalDialog

--- Paraject/MVVM/ViewModels/ModalDialogs/ProjectIdeaDetailsModalDialogViewModel.cs
+++ Paraject/MVVM/ViewModels/ModalDialogs/ProjectIdeaDetailsModalDialogViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Paraject.Core.Commands;
+using Paraject.Core.Repositories;
 using Paraject.MVVM.Models;
 using Paraject.MVVM.ViewModels.Windows;
 using System.Windows;
@@ -9,11 +10,16 @@ namespace Paraject.MVVM.ViewModels.ModalDialogs
     public class ProjectIdeaDetailsModalDialogViewModel : BaseViewModel
     {
         private readonly int _projectIdeaId;
+        private readonly ProjectIdeaRepository _projectIdeaRepository;
 
         public ProjectIdeaDetailsModalDialogViewModel(int projectIdeaId)
         {
+            _projectIdeaRepository = new ProjectIdeaRepository();
             _projectIdeaId = projectIdeaId;
+
             CloseModalDialogCommand = new DelegateCommand(CloseModalDialog);
+
+            CurrentProjectIdea = _projectIdeaRepository.Get(projectIdeaId);
         }
 
         #region Properties
