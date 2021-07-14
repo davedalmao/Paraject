7709669541b092654e36ae26ea86d9323e7e5db0
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-14 14:16:15 +0800

    implemented close modal functionality in AddProjectIdeaModalDialogViewModel

--- Paraject/MVVM/ViewModels/ModalDialogs/AddProjectIdeaModalDialogViewModel.cs
+++ Paraject/MVVM/ViewModels/ModalDialogs/AddProjectIdeaModalDialogViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Paraject.Core.Commands;
 using Paraject.MVVM.Models;
+using Paraject.MVVM.ViewModels.Windows;
 using System.Windows;
 using System.Windows.Input;
 
@@ -11,14 +12,32 @@ namespace Paraject.MVVM.ViewModels.ModalDialogs
         {
             CurrentProjectIdea = new ProjectIdea();
             AddProjectIdeaCommand = new DelegateCommand(Add);
+            CloseModalDialogCommand = new DelegateCommand(CloseModalDialog);
         }
 
+        #region Properties
         public ProjectIdea CurrentProjectIdea { get; set; }
         public ICommand AddProjectIdeaCommand { get; }
+        public ICommand CloseModalDialogCommand { get; }
+        #endregion
 
+        #region Methods
         private void Add()
         {
             MessageBox.Show($"Project Name: {CurrentProjectIdea.Name} \nDescription: {CurrentProjectIdea.Description} \nFeatures: {CurrentProjectIdea.Features}");
         }
+        private void CloseModalDialog()
+        {
+            MainWindowViewModel.Overlay = false;
+
+            foreach (Window currentModal in Application.Current.Windows)
+            {
+                if (currentModal.DataContext == this)
+                {
+                    currentModal.Close();
+                }
+            }
+        }
+        #endregion
     }
 }
