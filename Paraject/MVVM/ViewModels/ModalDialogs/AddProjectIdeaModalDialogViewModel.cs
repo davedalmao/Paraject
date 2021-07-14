42598c067037f1a8c5753ffbf29a6004fc902141
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-14 13:59:17 +0800

    Created AddProjectIdeaModalDialogViewModel and bound AddProjectIdeaModalDialogViewModel to AddProjectIdeaModalDialogView

---
+++ Paraject/MVVM/ViewModels/ModalDialogs/AddProjectIdeaModalDialogViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Paraject.Core.Commands;
+using Paraject.MVVM.Models;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels.ModalDialogs
+{
+    public class AddProjectIdeaModalDialogViewModel : BaseViewModel
+    {
+        public AddProjectIdeaModalDialogViewModel()
+        {
+            CurrentProjectIdea = new ProjectIdea();
+            AddProjectIdeaCommand = new DelegateCommand(Add);
+        }
+
+        public ProjectIdea CurrentProjectIdea { get; set; }
+        public ICommand AddProjectIdeaCommand { get; }
+
+        private void Add()
+        {
+            MessageBox.Show($"Project Name: {CurrentProjectIdea.Name} \nDescription: {CurrentProjectIdea.Description} \nFeatures: {CurrentProjectIdea.Features}");
+        }
+    }
+}
