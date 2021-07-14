1662a639fcbbb63eeb0f03f8c288b242d321e524
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-14 15:42:26 +0800

    created ProjectIdeaDetailsModalDialogViewModel and data bound ProjectIdeaDetailsModalDialog with ProjectIdeaDetailsModalDialogViewModel

---
+++ Paraject/MVVM/ViewModels/ModalDialogs/ProjectIdeaDetailsModalDialogViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Paraject.MVVM.Models;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels.ModalDialogs
+{
+    public class ProjectIdeaDetailsModalDialogViewModel : BaseViewModel
+    {
+        private readonly int _projectIdeaId;
+
+        public ProjectIdeaDetailsModalDialogViewModel(int projectIdeaId)
+        {
+            _projectIdeaId = projectIdeaId;
+        }
+
+        #region Properties
+        public ProjectIdea CurrentProjectIdea { get; set; }
+
+        public ICommand UpdateProjectIdeaCommand { get; }
+        public ICommand DeleteProjectIdeaCommand { get; }
+        #endregion
+    }
+}
