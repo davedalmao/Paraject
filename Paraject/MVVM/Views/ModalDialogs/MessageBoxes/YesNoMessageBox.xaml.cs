99f9dbd5cf22328deb0885fda247e56288a9321e
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-17 16:46:14 +0800

    finished creating the UI design for YesNoMessageBox

---
+++ Paraject/MVVM/Views/ModalDialogs/MessageBoxes/YesNoMessageBox.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Paraject.MVVM.Views.ModalDialogs.MessageBoxes
+{
+    /// <summary>
+    /// Interaction logic for YesNoMessageBox.xaml
+    /// </summary>
+    public partial class YesNoMessageBox : Window
+    {
+        public YesNoMessageBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
