48553a8bee483f461b299c5606666a439cd9d881
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-17 16:43:52 +0800

    finished creating the UI design for OkayMessageBox

---
+++ Paraject/MVVM/Views/ModalDialogs/MessageBoxes/OkayMessageBox.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.Views.ModalDialogs.MessageBoxes
+{
+    /// <summary>
+    /// Interaction logic for OkayMessageBox.xaml
+    /// </summary>
+    public partial class OkayMessageBox : Window
+    {
+        public OkayMessageBox()
+        {
+            InitializeComponent();
+            this.Owner = Application.Current.MainWindow;
+        }
+
+        private void OkayMessageBoxMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) { DragMove(); }
+        }
+    }
+}
