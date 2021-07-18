7f501bdea9ce5cc2f7df2f5655dedac33005fc7b
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-18 14:01:29 +0800

    DialogWindow created (the parent for all message boxes)

---
+++ Paraject/MVVM/Views/ModalDialogs/MessageBoxes/DialogWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Paraject.Core.Services.DialogService;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.Views.ModalDialogs.MessageBoxes
+{
+    /// <summary>
+    /// Interaction logic for DialogWindow.xaml
+    /// </summary>
+    public partial class DialogWindow : Window, IDialogWindow
+    {
+        public DialogWindow()
+        {
+            InitializeComponent();
+            this.Owner = Application.Current.MainWindow;
+        }
+
+        private void DialogWindowMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) { DragMove(); }
+        }
+    }
+}
