f88b1c35b66a3a71e4338bcadf74a09a104dc26c
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-18 13:49:10 +0800

    created IDialogWindow in DialogService folder

---
+++ Paraject/Core/Services/DialogService/IDialogWindow.cs
@@ -0,0 +1,9 @@
+﻿namespace Paraject.Core.Services.DialogService
+{
+    public interface IDialogWindow
+    {
+        bool? DialogResult { get; set; }
+        object DataContext { get; set; }
+        bool? ShowDialog();
+    }
+}
