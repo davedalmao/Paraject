4b61340b36ca394128097087fa382796e4cbd35d
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-18 13:53:59 +0800

    created DialogService class in DialogService folder

---
+++ Paraject/Core/Services/DialogService/DialogService.cs
@@ -0,0 +1,15 @@
+﻿using Paraject.MVVM.ViewModels.MessageBoxes;
+
+namespace Paraject.Core.Services.DialogService
+{
+    public class DialogService : IDialogService
+    {
+        public T OpenDialog<T>(DialogBaseViewModel<T> viewModel)
+        {
+            //IDialogWindow window = new DialogWindow();
+            //window.DataContext = viewModel;
+            //window.ShowDialog();
+            return viewModel.DialogResult;
+        }
+    }
+}
