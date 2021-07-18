ba40727dad179bcf058aab6665999b1fb32bbe05
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-18 14:07:51 +0800

    recreated OkayMessageBox as a UserControl (instead of as a Window, and deleted the old OkayMessageBox)

--- Paraject/MVVM/Views/ModalDialogs/MessageBoxes/OkayMessageBox.xaml.cs
+++ Paraject/MVVM/Views/ModalDialogs/MessageBoxes/OkayMessageBox.xaml.cs
@@ -1,22 +1,28 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Paraject.MVVM.Views.ModalDialogs.MessageBoxes
 {
     /// <summary>
     /// Interaction logic for OkayMessageBox.xaml
     /// </summary>
-    public partial class OkayMessageBox : Window
+    public partial class OkayMessageBox : UserControl
     {
         public OkayMessageBox()
         {
             InitializeComponent();
-            this.Owner = Application.Current.MainWindow;
-        }
-
-        private void OkayMessageBoxMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left) { DragMove(); }
         }
     }
 }
