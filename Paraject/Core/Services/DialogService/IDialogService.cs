4fdca9cd1c9f2f688b12d226ad434a5c14094ad9
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-18 13:50:41 +0800

    created IDialogService

---
+++ Paraject/Core/Services/DialogService/IDialogService.cs
@@ -0,0 +1,9 @@
+﻿using Paraject.MVVM.ViewModels.MessageBoxes;
+
+namespace Paraject.Core.Services.DialogService
+{
+    public interface IDialogService
+    {
+        T OpenDialog<T>(DialogBaseViewModel<T> viewModel);
+    }
+}
