47746b37844a52554725d11a2584874fc28dbe4d
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-18 17:06:44 +0800

    changed all MessageBoxes to OkayMessageBoxes in AddProjectModalDialogViewModel

--- Paraject/MVVM/ViewModels/ModalDialogs/AddProjectModalDialogViewModel.cs
+++ Paraject/MVVM/ViewModels/ModalDialogs/AddProjectModalDialogViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using Paraject.Core.Commands;
 using Paraject.Core.Repositories;
+using Paraject.Core.Services.DialogService;
 using Paraject.MVVM.Models;
+using Paraject.MVVM.ViewModels.MessageBoxes;
 using Paraject.MVVM.ViewModels.Windows;
 using System;
 using System.Drawing;
@@ -12,11 +14,13 @@ namespace Paraject.MVVM.ViewModels.ModalDialogs
 {
     public class AddProjectModalDialogViewModel : BaseViewModel
     {
+        private readonly IDialogService _dialogService;
         private readonly ProjectRepository _projectRepository;
         private readonly Action _refreshProjectsCollection;
 
         public AddProjectModalDialogViewModel(Action refreshProjectsCollection, int currentUserId)
         {
+            _dialogService = new DialogService();
             _projectRepository = new ProjectRepository();
             _refreshProjectsCollection = refreshProjectsCollection;
 
@@ -48,21 +52,30 @@ namespace Paraject.MVVM.ViewModels.ModalDialogs
 
             else
             {
-                MessageBox.Show("A project should have a name");
+                string iconSource = "/UiDesign/Images/Logo/defaultProjectLogo.png";
+
+                OkayMessageBoxViewModel okayMessageBox = new("Incorrect Data Entry", "A Project should have a name.", iconSource);
+                _dialogService.OpenDialog(okayMessageBox);
             }
         }
         private void AddOperationResult(bool isAdded)
         {
+            string iconSource = "/UiDesign/Images/Logo/defaultProjectLogo.png";
+
             if (isAdded)
             {
                 _refreshProjectsCollection();
-                MessageBox.Show("Project Created", "Title");
+
+                OkayMessageBoxViewModel okayMessageBox = new("Add Operation", "Project Created Successfully!", iconSource);
+                _dialogService.OpenDialog(okayMessageBox);
+
                 CloseModalDialog();
             }
 
             else
             {
-                MessageBox.Show("Error occured, cannot create project");
+                OkayMessageBoxViewModel okayMessageBox = new("Error", "An error occured, cannot create the Project.", iconSource);
+                _dialogService.OpenDialog(okayMessageBox);
             }
         }
 
@@ -84,7 +97,10 @@ namespace Paraject.MVVM.ViewModels.ModalDialogs
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show($"Please input a valid image.\n \n{ex}");
+                    string iconSource = "/UiDesign/Images/Logo/defaultProjectLogo.png";
+
+                    OkayMessageBoxViewModel okayMessageBox = new("Image format Error", $"Please select a valid image.\n \n{ex}", iconSource);
+                    _dialogService.OpenDialog(okayMessageBox);
                 }
             }
         }
