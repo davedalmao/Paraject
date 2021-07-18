16aaf7b6f1e8353b3c01e4469f5e14aa80501c75
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-18 14:27:42 +0800

    implemented OkayMessageBoxViewModel and bound it to OkayMessageBox

--- Paraject/MVVM/ViewModels/MessageBoxes/OkayMessageBoxViewModel.cs
+++ Paraject/MVVM/ViewModels/MessageBoxes/OkayMessageBoxViewModel.cs
@@ -1,27 +1,33 @@
 ﻿using Paraject.Core.Commands;
+using Paraject.Core.Enums;
+using Paraject.Core.Services.DialogService;
 using Paraject.Core.Utilities;
 using Paraject.MVVM.ViewModels.Windows;
 using System;
+using System.Windows.Input;
 
 namespace Paraject.MVVM.ViewModels.MessageBoxes
 {
-    public class OkayMessageBoxViewModel : ICloseWindows
+    public class OkayMessageBoxViewModel : DialogBaseViewModel<DialogResults>, ICloseWindows
     {
         private DelegateCommand _closeCommand;
 
-        public OkayMessageBoxViewModel(string title, string message)
+        public OkayMessageBoxViewModel(string title, string message) : base(message, title)
         {
             Title = title;
             Message = message;
-
+            OkayCommand = new RelayCommand<IDialogWindow>(Okay);
         }
 
-        public string Title { get; set; }
-        public string Message { get; set; }
 
         public Action Close { get; set; }
         public DelegateCommand CloseCommand => _closeCommand ??= new DelegateCommand(CloseWindow);
+        public ICommand OkayCommand { get; private set; }
 
+        private void Okay(IDialogWindow window)
+        {
+            CloseDialogWithResult(window, DialogResults.Okay);
+        }
         public void CloseWindow()
         {
             Close?.Invoke();
