519d6574c2a12bee6120abf8221fb3d2a7da1990
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-18 14:22:42 +0800

    implemented YesNoMessageBoxViewModel and bind it to YesNoMessageBox

--- Paraject/MVVM/ViewModels/MessageBoxes/YesNoMessageBoxViewModel.cs
+++ Paraject/MVVM/ViewModels/MessageBoxes/YesNoMessageBoxViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace Paraject.MVVM.ViewModels.MessageBoxes
+﻿using Paraject.Core.Commands;
+using Paraject.Core.Enums;
+using Paraject.Core.Services.DialogService;
+using System.Windows.Input;
+
+namespace Paraject.MVVM.ViewModels.MessageBoxes
 {
-    public class YesNoMessageBoxViewModel
+    public class YesNoMessageBoxViewModel : DialogBaseViewModel<DialogResults>
     {
+        public YesNoMessageBoxViewModel(string title, string message) : base(message, title)
+        {
+            YesCommand = new RelayCommand<IDialogWindow>(Yes);
+            NoCommand = new RelayCommand<IDialogWindow>(No);
+        }
+
+        public ICommand YesCommand { get; private set; }
+        public ICommand NoCommand { get; private set; }
+
+
+        private void Yes(IDialogWindow window)
+        {
+            CloseDialogWithResult(window, DialogResults.Yes);
+        }
+        private void No(IDialogWindow window)
+        {
+            CloseDialogWithResult(window, DialogResults.No);
+        }
     }
 }
