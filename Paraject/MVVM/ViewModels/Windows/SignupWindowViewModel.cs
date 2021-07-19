85f2138c91d2099eb11c09e8923875f25e6fb019
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-19 17:07:08 +0800

    changed all MessageBoxes to OkayMessageBoxes in SignupWindowViewModel

--- Paraject/MVVM/ViewModels/Windows/SignupWindowViewModel.cs
+++ Paraject/MVVM/ViewModels/Windows/SignupWindowViewModel.cs
@@ -1,21 +1,24 @@
 ﻿using Paraject.Core.Commands;
 using Paraject.Core.Repositories;
+using Paraject.Core.Services.DialogService;
 using Paraject.MVVM.Models;
+using Paraject.MVVM.ViewModels.MessageBoxes;
 using Paraject.MVVM.Views.Windows;
 using System;
-using System.Windows;
 using System.Windows.Input;
 
 namespace Paraject.MVVM.ViewModels.Windows
 {
     public class SignupWindowViewModel : BaseViewModel
     {
+        private readonly IDialogService _dialogService;
         private readonly UserAccountRepository _userAccountRepository;
         private DelegateCommand _loginWindowRedirectCommand;
         public event EventHandler Closed; //The Window (LoginWindow) closes itself when this event is executed
 
         public SignupWindowViewModel()
         {
+            _dialogService = new DialogService();
             _userAccountRepository = new UserAccountRepository();
             AddCommand = new DelegateCommand(Add);
             CurrentUserAccount = new UserAccount();
@@ -24,32 +27,24 @@ namespace Paraject.MVVM.ViewModels.Windows
         #region Properties
         public string InitialPassword { get; set; }
         public UserAccount CurrentUserAccount { get; set; }
+
         public ICommand AddCommand { get; }
-        public ICommand LoginWindowRedirectCommand
-        {
-            get { return _loginWindowRedirectCommand ??= new DelegateCommand(ShowLoginWindow); }
-        }
+        public ICommand LoginWindowRedirectCommand => _loginWindowRedirectCommand ??= new DelegateCommand(ShowLoginWindow);
         #endregion
 
-        #region Add UserAccount Methods
+        #region Methods
         private void Add()
         {
-            try
+            if (ValidateInput())
             {
-                if (ValidateInput())
-                {
-                    AddUserAccountToDatabase();
-                }
+                AddUserAccountToDatabase();
+            }
 
-                else
-                {
-                    MessageBox.Show("Check your inputs: \n1. Passwords doesn't match \n2. No username in the input");
-                }
-            }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.ToString());
+                _dialogService.OpenDialog(new OkayMessageBoxViewModel("Incorrect Data Entry", "Check your inputs: \n\n1. Passwords doesn't match \n2. No username in the input", "/UiDesign/Images/Logo/defaultProjectLogo.png"));
             }
+
         }
         private void AddUserAccountToDatabase()
         {
@@ -63,16 +58,16 @@ namespace Paraject.MVVM.ViewModels.Windows
         private bool ValidateInput()
         {
             bool isValid = false;
-            // Check if Username, initial Password, and confirm Password is not blank
-            if (!string.IsNullOrWhiteSpace(CurrentUserAccount.Username) && !string.IsNullOrWhiteSpace(InitialPassword) && !string.IsNullOrWhiteSpace(CurrentUserAccount.Password))
+
+            if (!string.IsNullOrWhiteSpace(CurrentUserAccount.Username) &&
+                !string.IsNullOrWhiteSpace(InitialPassword) &&
+                !string.IsNullOrWhiteSpace(CurrentUserAccount.Password))
             {
                 return InitialPassword.Equals(CurrentUserAccount.Password); //Check if initial password and confirm password is the same
             }
             return isValid;
         }
-        #endregion
 
-        #region Specific Window Methods
         private void ShowLoginWindow()
         {
             LoginWindow loginWindow = new();
@@ -81,12 +76,15 @@ namespace Paraject.MVVM.ViewModels.Windows
         }
         private void ShowMainWindow()
         {
+            _dialogService.OpenDialog(new OkayMessageBoxViewModel("Welcome", "Account Created Successfully! \nPress Okay to Start!", "/UiDesign/Images/Logo/defaultProjectLogo.png"));
+
             UserAccount userToLogin = _userAccountRepository.GetByUsername(CurrentUserAccount.Username);
             MainWindow mainWindow = new(userToLogin);
             mainWindow.Show();
             Close(); //Closes SignupWindow when MainWindow is present
         }
-        private void Close() //The method that executes Closed EventHandler
+
+        private void Close()
         {
             Closed?.Invoke(this, EventArgs.Empty);
         }
