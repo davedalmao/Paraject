5dc19c1d7d4b7d4ab01194f91622063349920f0c
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-19 16:46:12 +0800

    changed the MessageBox to OkayMessageBox in LoginWindowViewModel

--- Paraject/MVVM/ViewModels/Windows/LoginWindowViewModel.cs
+++ Paraject/MVVM/ViewModels/Windows/LoginWindowViewModel.cs
@@ -1,36 +1,37 @@
 ﻿using Paraject.Core.Commands;
 using Paraject.Core.Repositories;
+using Paraject.Core.Services.DialogService;
 using Paraject.MVVM.Models;
+using Paraject.MVVM.ViewModels.MessageBoxes;
 using Paraject.MVVM.Views.Windows;
 using System;
-using System.Windows;
 using System.Windows.Input;
 
 namespace Paraject.MVVM.ViewModels.Windows
 {
-    class LoginWindowViewModel : BaseViewModel
+    public class LoginWindowViewModel : BaseViewModel
     {
+        private readonly IDialogService _dialogService;
         private readonly UserAccountRepository _userAccountRepository;
         private DelegateCommand _signUpWindowRedirectCommand;
         public event EventHandler Closed; //The Window (LoginWindow) closes itself when this event is executed
 
         public LoginWindowViewModel()
         {
+            _dialogService = new DialogService();
             _userAccountRepository = new UserAccountRepository();
             CurrentUserAccount = new UserAccount();
             LoginCommand = new DelegateCommand(Login);
         }
 
         #region Properties
-        public DelegateCommand SignUpWindowRedirectCommand
-        {
-            get { return _signUpWindowRedirectCommand ??= new DelegateCommand(ShowSignupWindow); }
-        }
         public UserAccount CurrentUserAccount { get; set; }
+
+        public ICommand SignUpWindowRedirectCommand => _signUpWindowRedirectCommand ??= new DelegateCommand(ShowSignupWindow);
         public ICommand LoginCommand { get; }
         #endregion 
 
-        #region Login Methods
+        #region Methods
         private void Login()
         {
             if (ValidateInput())
@@ -40,7 +41,7 @@ namespace Paraject.MVVM.ViewModels.Windows
 
             else
             {
-                MessageBox.Show("Check your inputs: username does not exist, or incorrect password.");
+                _dialogService.OpenDialog(new OkayMessageBoxViewModel("Incorrect Data Entry", "Check your inputs: Username does not exist, or incorrect Password.", "/UiDesign/Images/Logo/defaultProjectLogo.png"));
             }
         }
         private bool ValidateInput()
@@ -51,9 +52,7 @@ namespace Paraject.MVVM.ViewModels.Windows
             }
             return false;
         }
-        #endregion
 
-        #region Specific Window Methods
         private void ShowSignupWindow()
         {
             SignupWindow signupWindow = new SignupWindow();
@@ -67,7 +66,8 @@ namespace Paraject.MVVM.ViewModels.Windows
             mainWindow.Show();
             Close(); //Closes LoginWindow when MainWindow is present
         }
-        private void Close() //The method that executes Closed EventHandler
+
+        private void Close()
         {
             Closed?.Invoke(this, EventArgs.Empty);
         }
