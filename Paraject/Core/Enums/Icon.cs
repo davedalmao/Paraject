0e57159c6ae17d5efa56b07cb00806ac92aae19e
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-20 16:51:11 +0800

    created Icon enum, that stores the filepath for all custom MessageBox Icons

---
+++ Paraject/Core/Enums/Icon.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+
+namespace Paraject.Core.Enums
+{
+    /// <summary>
+    /// Stores the filepath for custom MessageBox Icons
+    /// </summary>
+    public enum Icon
+    {
+        //User
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        User,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        ValidUser,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        InvalidUser,
+
+        //ProjectIdea
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        ProjectIdea,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        ValidProjectIdea,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        InvalidProjectIdea,
+
+        //Project
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        Project,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        ValidProject,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        InvalidProject,
+
+        //Note
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        Note,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        ValidNote,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        InvalidNote,
+
+        //Task
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        Task,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        ValidTask,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        InvalidTask,
+
+        //Subtask
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        Subtask,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        ValidSubtask,
+        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        InvalidSubtask
+    }
+}
