cb828136f5e8e6ea9c05183cf753a2af1ade7d5a
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-21 13:34:40 +0800

    assigned the proper description for each enum member in Icon enum

--- Paraject/Core/Enums/Icon.cs
+++ Paraject/Core/Enums/Icon.cs
@@ -8,51 +8,51 @@ namespace Paraject.Core.Enums
     public enum Icon
     {
         //User
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/User.svg")]
         User,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/ValidUser.svg")]
         ValidUser,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/InvalidUser.svg")]
         InvalidUser,
 
         //ProjectIdea
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/ProjectIdea.svg")]
         ProjectIdea,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/ValidProjectIdea.svg")]
         ValidProjectIdea,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/InvalidProjectIdea.svg")]
         InvalidProjectIdea,
 
         //Project
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/Project.svg")]
         Project,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/ValidProject.svg")]
         ValidProject,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/InvalidProject.svg")]
         InvalidProject,
 
         //Note
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/Note.svg")]
         Note,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/ValidNote.svg")]
         ValidNote,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/InvalidNote.svg")]
         InvalidNote,
 
         //Task
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/Task.svg")]
         Task,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/ValidTask.svg")]
         ValidTask,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/InvalidTask.svg")]
         InvalidTask,
 
         //Subtask
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/Subtask.svg")]
         Subtask,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/ValidSubtask.svg")]
         ValidSubtask,
-        [Description("/UiDesign/Images/Logo/defaultProjectLogo.png")]
+        [Description("/UiDesign/Icons/MessageBox/InvalidSubtask.svg")]
         InvalidSubtask
     }
 }
