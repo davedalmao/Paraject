34fbcd66654221de0682ca6c618a00d64c7ddfed
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-21 16:44:49 +0800

    created CloseModalDialogCommand in Commands folder (to close a modal dialog if esc key is pressed)

---
+++ Paraject/Core/Commands/CloseModalDialogCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Paraject.Core.Commands
+{
+    public class CloseModalDialogCommand : ICommand
+    {
+
+        public bool CanExecute(object parameter)
+        {
+            return parameter is Window; //we can only close Windows
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                ((Window)parameter).Close();
+            }
+        }
+
+        public event EventHandler CanExecuteChanged { add { } remove { } }
+
+        public static readonly ICommand Instance = new CloseModalDialogCommand();
+    }
+}
