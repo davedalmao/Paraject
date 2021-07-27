fbe9c480f4eb5546b2bdaa94e54cc6a05700fffa
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-27 14:16:39 +0800

    Renamed AllTasksView to TasksView

---
+++ Paraject/MVVM/Views/TasksView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Paraject.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for AllTasksView.xaml
+    /// </summary>
+    public partial class TasksView : UserControl
+    {
+        public TasksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
