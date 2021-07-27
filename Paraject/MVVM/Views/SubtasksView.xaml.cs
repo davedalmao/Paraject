a36445bd717a43c401d81c1b8121caa300e7784d
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-27 14:27:11 +0800

    Renamed AllSubtasksView to SubtasksView

---
+++ Paraject/MVVM/Views/SubtasksView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Paraject.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for SubtasksView.xaml
+    /// </summary>
+    public partial class SubtasksView : UserControl
+    {
+        public SubtasksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
