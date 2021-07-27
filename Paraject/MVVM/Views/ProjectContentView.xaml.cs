280365b8ae46c2f2339a7850ee5c4b7526a6a572
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-27 14:09:55 +0800

    Renamed TasksView to ProjectContentView

---
+++ Paraject/MVVM/Views/ProjectContentView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Paraject.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for TasksView.xaml
+    /// </summary>
+    public partial class ProjectContentView : UserControl
+    {
+        public ProjectContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
