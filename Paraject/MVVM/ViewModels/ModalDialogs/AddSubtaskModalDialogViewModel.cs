70389c4fb42872c5a4fc8e9ebbfce8b79661dc31
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-29 15:12:30 +0800

    date format is now user friendly when displaying an error message if the date is invalid in AddSubtaskModalDialogViewModel

--- Paraject/MVVM/ViewModels/ModalDialogs/AddSubtaskModalDialogViewModel.cs
+++ Paraject/MVVM/ViewModels/ModalDialogs/AddSubtaskModalDialogViewModel.cs
@@ -72,7 +72,7 @@ namespace Paraject.MVVM.ViewModels.ModalDialogs
 
             else if (SubtaskDeadlineIsValid() == false)
             {
-                _dialogService.OpenDialog(new OkayMessageBoxViewModel("Invalid Deadline Date", $"The selected date is invalid. Cannot create a new Subtask. \n\nThe deadline date should be within \n{ParentTask.DateCreated:d} - {ParentTask.Deadline:d} range, or not have a deadline for this subtask at all.", Icon.InvalidSubtask));
+                _dialogService.OpenDialog(new OkayMessageBoxViewModel("Invalid Deadline Date", $"The selected date is invalid. Cannot create a new Subtask. \n\nThe deadline date should be within \n{ParentTask.DateCreated:MMMM dd, yyyy} - {ParentTask.Deadline:MMMM dd, yyyy} range, or not have a deadline for this subtask at all.", Icon.InvalidSubtask));
                 return false;
             }
 
