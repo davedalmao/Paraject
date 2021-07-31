9d2834bead7059e79505f92f391474ef0e4f27b2
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-31 16:17:50 +0800

    massive g.i.cs changes

--- Paraject/obj/Debug/net5.0-windows/MVVM/Views/TaskContentView.g.i.cs
+++ Paraject/obj/Debug/net5.0-windows/MVVM/Views/TaskContentView.g.i.cs
@@ -55,7 +55,7 @@ namespace Paraject.MVVM.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Paraject;component/mvvm/views/taskcontentview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Paraject;V1.0.0.0;component/mvvm/views/taskcontentview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\Views\TaskContentView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
