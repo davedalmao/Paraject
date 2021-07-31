101203b8d711fab9ccab43db390ed16f37690b68
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-31 17:20:23 +0800

    deleted CamelCaseConverter in Converters folder, since it is never used

--- Paraject/obj/Debug/net5.0-windows/MVVM/Views/OptionsView.g.i.cs
+++ Paraject/obj/Debug/net5.0-windows/MVVM/Views/OptionsView.g.i.cs
@@ -53,7 +53,7 @@ namespace Paraject.MVVM.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Paraject;V1.0.0.0;component/mvvm/views/optionsview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Paraject;component/mvvm/views/optionsview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\Views\OptionsView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
