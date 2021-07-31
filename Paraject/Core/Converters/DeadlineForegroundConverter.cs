e45fd4a6295a3b9af81c98406349deed74575b07
John Dave Dalmao <johndavedalmao@gmail.com>
2021-07-31 15:24:57 +0800

    changed the deadline date foreground if it is past the deadline date (in TasksView)

---
+++ Paraject/Core/Converters/DeadlineForegroundConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Paraject.Core.Converters
+{
+    /// <summary>
+    /// If A Project, Task, and Subtask is past the deadline, then change the Deadline foreground, and icon to Red.
+    /// </summary>
+    public class DeadlineForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not DateTime) { return false; }
+
+            return DateTime.Now.Date > (DateTime)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
