6161ed1ac4b4a348611bca02b48c4bbf474cd0a2
John Dave Dalmao <johndavedalmao@gmail.com>
2021-08-01 15:03:15 +0800

    changed connection string in app.config, and ConnectionString class in Utilities folder

--- Paraject/obj/Release/net5.0-windows/Core/CustomControls/UserControls/BindablePasswordBox.g.i.cs
+++ Paraject/obj/Release/net5.0-windows/Core/CustomControls/UserControls/BindablePasswordBox.g.i.cs
@@ -55,13 +55,14 @@ namespace Paraject.Core.CustomControls.UserControls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Paraject;component/core/customcontrols/usercontrols/bindablepasswordbox.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Paraject;V1.0.0.0;component/core/customcontrols/usercontrols/bindablepasswordbox" +
+                    ".xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\Core\CustomControls\UserControls\BindablePasswordBox.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +72,7 @@ namespace Paraject.Core.CustomControls.UserControls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
