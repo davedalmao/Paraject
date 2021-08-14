6a3393d7d61badc242d4576ac73f1a35d6a12c1c
John Dave Dalmao <johndavedalmao@gmail.com>
2021-08-14 14:55:34 +0800

    added the new connectionstring to ConnectionString utility class (to point to the database's relative file path)

--- Paraject/Core/Utilities/ConnectionString.cs
+++ Paraject/Core/Utilities/ConnectionString.cs
@@ -1,9 +1,11 @@
-﻿using System.Configuration;
+﻿using System.IO;
+using System.Reflection;
 
 namespace Paraject.Core.Utilities
 {
     public static class ConnectionString
     {
-        public static readonly string config = ConfigurationManager.ConnectionStrings["ParajectDb"].ConnectionString;
+        public static readonly string installPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static readonly string config = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={installPath}\Core\Database\ParajectDb.mdf;Integrated Security=True";
     }
 }
